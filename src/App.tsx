f66f8b96afa5135cc79c7bcee03b8728121ad45e
K1rr1 <kiriakos.it.karipidis@gmail.com>
2026-03-12 13:36:30 +0100

    Feature
    Lagt in en loginskärm(dummy)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
 import MapView from "./components/map/MapView";
+import StartScreen from "./components/auth/StartScreen";
+import type { User } from "./features/auth/auth.types";
 
 export default function App() {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [nameInput, setNameInput] = useState("");
+
+  const handleLogin = () => {
+    const trimmedName = nameInput.trim();
+
+    if (!trimmedName) return;
+
+    setCurrentUser({
+      mode: "user",
+      name: trimmedName,
+    });
+
+    setNameInput("");
+  };
+
+  const handleContinueAsGuest = () => {
+    setCurrentUser({
+      mode: "guest",
+      name: "Gäst",
+    });
+  };
+
+  if (!currentUser) {
+    return (
+      <StartScreen
+        nameInput={nameInput}
+        onNameChange={setNameInput}
+        onLogin={handleLogin}
+        onContinueAsGuest={handleContinueAsGuest}
+      />
+    );
+  }
+
   return <MapView />;
 }
---
+++ src/features/auth/auth.types.ts
@@ -0,0 +1,7 @@
+export type UserMode = "guest" | "user";
+
+export type User= {
+    mode: UserMode;
+    name: string;
+
+};
